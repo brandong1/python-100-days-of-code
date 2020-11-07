c0bc08ebd7a9c725effd0f2ff0c0d15ef5ad8bf7
Brandon Green <brandon@brandonleeproductions.com>
2020-11-07 12:34:22 -0700

    📦 NEW: practice using random module

---
+++ Day 4/random_exercise.py
@@ -0,0 +1,11 @@
+# Remember to use the random module 👇
+import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
