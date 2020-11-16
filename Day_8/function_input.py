83aa301d1f405444cc7faaf9533b62c53b501c6b
Brandon Green <brandon@brandonleeproductions.com>
2020-11-15 23:30:59 -0700

    function input example

---
+++ Day_8/function_input.py
@@ -0,0 +1,23 @@
+# def greet():
+#     print("Hello")
+#     print("Hi there!")
+#     print("What up")
+
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello, {name}!")
+#     print(f"Hi there, {name}!")
+#     print(f"What up, {name}!")
+
+
+# greet_with_name("Brandon")
+
+# Functions with more than one input
+def greet_with(name, location):
+    print(f"Hello, {name}")
+    print(f"What's it like in {location}?")
+
+
+greet_with(name="Brandon", location="Denver")
